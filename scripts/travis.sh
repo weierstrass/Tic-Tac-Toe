bc9426c9276f6737d706370a2382346da69f1d96
ShulKriid <30507525+ShulKriid@users.noreply.github.com>
2017-08-17 22:23:23 +0300

    Finalise and separate the scripts.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
 saveCurrentFolder=`pwd`
-echo "Compilation started."
 
-echo "Step 1: Build folder creation."
-if [ ! -d "../build" ]; then
-	mkdir ../build;
-fi
-cd ../build
+./build.sh
 
-echo "Step 2: Run CMake."
-cmake ..
-
-echo "Step 3: Run make."
-make
+echo "Step 4: Run Unit Tests."
+cd test
+./unitTest
 
 cd $saveCurrentFolder
